f499cbc4b905460412af54f03dbc80c73f30c0bc
Fozat Kardouh <fozatkardouh@gmail.com>
2018-04-17 16:22:58 +0200

    added empty project

---
+++ src/main/java/at/refugeescode/mp15_pirates_of_the_code_bean/Mp15PiratesOfTheCodeBeanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp15_pirates_of_the_code_bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp15PiratesOfTheCodeBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp15PiratesOfTheCodeBeanApplication.class, args);
+	}
+}
